b74272942fe565744d8e5c033f00445a3d6d6a7a
Phung-B2005729 <phungb2005729@student.ctu.edu.vn>
2023-09-15 23:06:03 +0700

    chinh sua

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
 
-const PORT =  config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server in runnig on port ${PORT}.`);
-});
+async function startServer(){
+    try {
+        // ket noi mongodb
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database");
+
+
+        //
+        const PORT =  config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server in runnig on port ${PORT}.`);
+        });
+
+    }
+    catch(err){
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    }
+}
+
+startServer();
+
